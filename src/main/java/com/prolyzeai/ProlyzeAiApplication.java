a71c5069fd743fdbd5dd1352efa631ab9963cf48
Can Deniz Gumus <denizgumus1996@gmail.com>
2025-09-06 15:43:34 +0300

    1

---
+++ src/main/java/com/prolyzeai/ProlyzeAiApplication.java
@@ -0,0 +1,15 @@
+package com.prolyzeai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProlyzeAiApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ProlyzeAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prolyzeai/ProlyzeAiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.prolyzeai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProlyzeAiApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
